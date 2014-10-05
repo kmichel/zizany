7f699537314112d6da2e8661a203ec8e6d0cbd71
Kevin Michel <kmichel.info@gmail.com>
2014-10-05 19:06:11 +0200

    Check status code from formatting and output functions in json_writer

--- src/json_writer.cpp
+++ src/json_writer.cpp
@@ -1,30 +1,40 @@
 #include "json_writer.hpp"
 
-#include <limits>
-#include <cmath>
-#include <cinttypes>
-
 #include "../libs/gdtoa/gdtoa.h"
 
-// TODO: test putc_unlocked (EOF), snprintf, g_ffmt and g_dfmt (length if unlimited) output
+#include <limits>
+#include <cinttypes>
+#include <cmath>
+#include <cstring>
+#include <stdexcept>
+#include <sys/errno.h>
+
 namespace zizany {
+    static
+    void
+    putc_unlocked_checked(int c, FILE *output) {
+        const int putc_status(putc_unlocked(c, output));
+        if (putc_status == EOF)
+            throw std::runtime_error(strerror(errno));
+    }
+
     static
     void
     fputs_unlocked_(const char *str, FILE *output) {
         while (*str != 0)
-            putc_unlocked(*str++, output);
+            putc_unlocked_checked(*str++, output);
     }
 
     static
     void
     print_with_leading_zero(const char *buffer, FILE *output) {
         if (buffer[0] == '-' && buffer[1] == '.') {
-            putc_unlocked('-', output);
-            putc_unlocked('0', output);
+            putc_unlocked_checked('-', output);
+            putc_unlocked_checked('0', output);
             fputs_unlocked_(buffer + 1, output);
         } else {
             if (buffer[0] == '.')
-                putc_unlocked('0', output);
+                putc_unlocked_checked('0', output);
             fputs_unlocked_(buffer, output);
         }
     }
@@ -54,8 +64,10 @@ namespace zizany {
     void
     json_writer::add_number(std::int32_t value) {
         insert_separator_if_needed();
-        char buffer[64];
-        snprintf(buffer, 64, "%" PRIi32, value);
+        char buffer[32];
+        const int snprintf_status(snprintf(buffer, sizeof(buffer), "%" PRIi32, value));
+        if (snprintf_status >= static_cast<int>(sizeof(buffer)))
+            throw std::runtime_error("unexpected large number");
         fputs_unlocked_(buffer, output);
         state = writer_state::after_value;
     }
@@ -63,8 +75,10 @@ namespace zizany {
     void
     json_writer::add_number(std::uint32_t value) {
         insert_separator_if_needed();
-        char buffer[64];
-        snprintf(buffer, 64, "%" PRIu32, value);
+        char buffer[32];
+        const int snprintf_status(snprintf(buffer, sizeof(buffer), "%" PRIu32, value));
+        if (snprintf_status >= static_cast<int>(sizeof(buffer)))
+            throw std::runtime_error("unexpected large number");
         fputs_unlocked_(buffer, output);
         state = writer_state::after_value;
     }
@@ -72,8 +86,10 @@ namespace zizany {
     void
     json_writer::add_number(std::int64_t value) {
         insert_separator_if_needed();
-        char buffer[64];
-        snprintf(buffer, 64, "%" PRIi64, value);
+        char buffer[32];
+        const int snprintf_status(snprintf(buffer, 32, "%" PRIi64, value));
+        if (snprintf_status >= static_cast<int>(sizeof(buffer)))
+            throw std::runtime_error("unexpected large number");
         fputs_unlocked_(buffer, output);
         state = writer_state::after_value;
     }
@@ -81,8 +97,10 @@ namespace zizany {
     void
     json_writer::add_number(std::uint64_t value) {
         insert_separator_if_needed();
-        char buffer[64];
-        snprintf(buffer, 64, "%" PRIu64, value);
+        char buffer[32];
+        const int snprintf_status(snprintf(buffer, 32, "%" PRIu64, value));
+        if (snprintf_status >= static_cast<int>(sizeof(buffer)))
+            throw std::runtime_error("unexpected large number");
         fputs_unlocked_(buffer, output);
         state = writer_state::after_value;
     }
@@ -94,7 +112,9 @@ namespace zizany {
             fputs_unlocked_("null", output);
         else {
             char buffer[32];
-            g_ffmt(buffer, &value, -1, sizeof(buffer));
+            const char *g_ffmt_status(g_ffmt(buffer, &value, -1, sizeof(buffer)));
+            if (g_ffmt_status == nullptr)
+                throw std::runtime_error("unexpected large number");
             print_with_leading_zero(buffer, output);
         }
         state = writer_state::after_value;
@@ -107,7 +127,9 @@ namespace zizany {
             fputs_unlocked_("null", output);
         else {
             char buffer[32];
-            g_dfmt(buffer, &value, -1, sizeof(buffer));
+            const char *g_dfmt_status(g_dfmt(buffer, &value, -1, sizeof(buffer)));
+            if (g_dfmt_status == nullptr)
+                throw std::runtime_error("unexpected large number");
             print_with_leading_zero(buffer, output);
         }
         state = writer_state::after_value;
@@ -172,7 +194,7 @@ namespace zizany {
     void
     json_writer::start_composite(char marker, bool force_inline) {
         insert_separator_if_needed();
-        putc_unlocked(marker, output);
+        putc_unlocked_checked(marker, output);
         if (force_inline)
             inline_level = std::min(inline_level, indent_level);
         indent_level += 1;
@@ -184,7 +206,7 @@ namespace zizany {
         indent_level -= 1;
         if (state == writer_state::after_value && indent_level < inline_level)
             insert_newline();
-        putc_unlocked(marker, output);
+        putc_unlocked_checked(marker, output);
         state = writer_state::after_value;
         if (indent_level == inline_level)
             inline_level = std::numeric_limits<int>::max();
@@ -193,20 +215,20 @@ namespace zizany {
     void
     json_writer::insert_separator_if_needed() {
         if (state == writer_state::after_value)
-            putc_unlocked(',', output);
+            putc_unlocked_checked(',', output);
         if (state != writer_state::after_key) {
             if (state != writer_state::after_start && indent_level < inline_level)
                 insert_newline();
             else if (state == writer_state::after_value)
-                putc_unlocked(' ', output);
+                putc_unlocked_checked(' ', output);
         }
     }
 
     void
     json_writer::insert_newline() {
-        putc_unlocked('\n', output);
+        putc_unlocked_checked('\n', output);
         for (int i = 0; i < indent_width * indent_level; ++i)
-            putc_unlocked(' ', output);
+            putc_unlocked_checked(' ', output);
     }
 
     inline
@@ -214,68 +236,68 @@ namespace zizany {
         const char hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         switch (character) {
             case '\b':
-                putc_unlocked('\\', output);
-                putc_unlocked('b', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('b', output);
                 break;
             case '\f':
-                putc_unlocked('\\', output);
-                putc_unlocked('f', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('f', output);
                 break;
             case '\n':
-                putc_unlocked('\\', output);
-                putc_unlocked('n', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('n', output);
                 break;
             case '\r':
-                putc_unlocked('\\', output);
-                putc_unlocked('r', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('r', output);
                 break;
             case '\t':
-                putc_unlocked('\\', output);
-                putc_unlocked('t', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('t', output);
                 break;
             case '\\':
-                putc_unlocked('\\', output);
-                putc_unlocked('\\', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('\\', output);
                 break;
             case '\"':
-                putc_unlocked('\\', output);
-                putc_unlocked('"', output);
+                putc_unlocked_checked('\\', output);
+                putc_unlocked_checked('"', output);
                 break;
             default:
                 if (character > 0 && character < 32) {
-                    putc_unlocked('\\', output);
-                    putc_unlocked('u', output);
-                    putc_unlocked('0', output);
-                    putc_unlocked('0', output);
-                    putc_unlocked(hex[character >> 4], output);
-                    putc_unlocked(hex[character & 0xf], output);
+                    putc_unlocked_checked('\\', output);
+                    putc_unlocked_checked('u', output);
+                    putc_unlocked_checked('0', output);
+                    putc_unlocked_checked('0', output);
+                    putc_unlocked_checked(hex[character >> 4], output);
+                    putc_unlocked_checked(hex[character & 0xf], output);
                 } else {
-                    putc_unlocked(character, output);
+                    putc_unlocked_checked(character, output);
                 }
         }
     }
 
     void
     json_writer::print_quoted_string(const std::string &string) {
-        putc_unlocked('"', output);
+        putc_unlocked_checked('"', output);
         for (std::size_t index = 0; index < string.size(); ++index)
             print_quoted_character(output, string[index]);
-        putc_unlocked('"', output);
+        putc_unlocked_checked('"', output);
     }
 
     void
     json_writer::print_quoted_string(const char *string) {
-        putc_unlocked('"', output);
+        putc_unlocked_checked('"', output);
         while (*string != 0)
             print_quoted_character(output, *string++);
-        putc_unlocked('"', output);
+        putc_unlocked_checked('"', output);
     }
 
     void
     json_writer::print_quoted_string(const char *string, const std::size_t length) {
-        putc_unlocked('"', output);
+        putc_unlocked_checked('"', output);
         for (std::size_t index = 0; index < length; ++index)
             print_quoted_character(output, string[index]);
-        putc_unlocked('"', output);
+        putc_unlocked_checked('"', output);
     }
 }
